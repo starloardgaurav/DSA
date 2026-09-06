d21a5499097f0f205921f9e9a89d4d19b74b7627
Gaurav Kumar <starloardgaurav@gmail.com>
2026-09-06 22:03:30 +0530

    Implement addTwoNumbers method for linked lists

---
+++ Linked-List/Add-Two-Numbers-2/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        ListNode l3 = new ListNode(0);
+        ListNode curr = l3;
+      
+        int carry = 0;
+        
+        while(l1 != null || l2 != null || carry != 0){
+            int sum = carry;
+
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            curr.next = new ListNode(digit);
+            curr = curr.next;
+        }
+        
+        return l3.next;
+    }
+}
